d2f400719ea5b8af0e13de9fbd791dbe917cf1f5
George Mamaladze <gmamaladze@gmail.com>
2016-09-03 12:01:59 +0200

    Protocol implementation seems to be ok.

--- IrToy/Program.cs
+++ IrToy/Program.cs
@@ -7,15 +7,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace IrToy
+namespace IrToy1
 {
+
     class Program
     {
 
         private static string Execute(SerialPort port, string command)
         {
             port.Write(command);
-            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+            Task.Delay(TimeSpan.FromMilliseconds(100)).Wait();
             var response = port.ReadExisting();
             return response;
         }
@@ -27,72 +28,66 @@ namespace IrToy
             {
                 Console.WriteLine($"[{i+1}] {ports[i]}");
             }
-            Console.WriteLine("Press port number or any other key to exit.");
-            var pressed = Console.ReadKey(true);
-            Console.WriteLine();
-            var ch = pressed.KeyChar;
-            int index;
-            var isOk = int.TryParse(ch.ToString(), out index);
-            if (!isOk && index>ports.Length) return;
 
-            var portName = ports[index-1];
-            using (var port = new SerialPort(portName, 115200))
+            var portName = ports[0];
+            using (var port = new SerialPort(portName))
             {
                 port.Open();
 
-                var init = new byte[] { 0xFF, 0x00 };
-                port.Write(init, 0, 1);
-                port.Write(init, 0, 1);
-                port.Write(init, 1, 1);
-                port.Write(init, 1, 1);
-                port.Write(init, 1, 1);
-                port.Write(init, 1, 1);
-                port.Write(init, 1, 1);
+                var init = new byte[] { 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00 };
+                port.Write(init, 0, init.Length);
 
                 Console.WriteLine($"Version: {Execute(port, "v")}");
                 Console.WriteLine($"Test: {Execute(port, "t")}");
-                Console.WriteLine($"Protocol: {Execute(port, "S")}");
-
-                var queue = new ConcurrentQueue<byte>();
-                using (var ctx = new CancellationTokenSource())
+                Console.WriteLine($"Handshake: {Execute(port, "r")}");
+                Console.WriteLine($"Enter sampling mode: {Execute(port, "s")}");
+                port.Write(new byte[] { 0x24, 0x25, 0x26 }, 0, 3); // Enable advanced TX features (no reply)
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Recording....");
+                var endOfSiganl = false;
+                var stack = new Stack<byte>();
+                while (!endOfSiganl)
                 {
-                    var receiver = Task.Factory.StartNew(() =>
+                    var received = port.ReadByte();
+                    if (received != -1)
                     {
-                        while (true)
-                        {
-                            var received = port.ReadByte();
-                            if (received != -1)
-                            {
-                                queue.Enqueue((byte)received);
-                                Console.Write(received);
-                            }
-                        }
-                    }, ctx.Token);
-
-                    ConsoleKey key = ConsoleKey.NoName;
-                    while (key != ConsoleKey.Q)
-                    {
-                        key = Console.ReadKey(true).Key;
-                        switch (key)
-                        {
-                            case ConsoleKey.R:
-                                while (queue.Count > 0)
-                                {
-                                    byte _;
-                                    queue.TryDequeue(out _);
-                                }
-                                break;
-                            case ConsoleKey.P:
-                                Console.WriteLine();
-                                Console.WriteLine("Sending ...");
-                                var buffer = queue.ToArray();
-                                port.Write(init, 1, 1);
-                                port.Write(new byte[] { 0x03 }, 0, 1);
-                                port.Write(buffer, 0, buffer.Length);
-                                break;
-                        }
+                        endOfSiganl = (received == 0xFF) && (stack.Peek() == 0xFF);
+                        stack.Push((byte)received);
                     }
                 }
+                stack.Pop();
+                stack.Pop();
+                var mesage = stack.ToArray();
+                Console.WriteLine($"Recorded {mesage.Length} bytes.");
+
+
+                Console.WriteLine("Pess any key to send. Press ESC to quit.");
+
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    return;
+
+                Console.WriteLine("---------------------------------");
+                port.Write(new byte[] { 0x03 }, 0, 1); //start
+
+                int offset = 0;
+                while (true)
+                {
+                    var bufferSize = port.ReadByte();
+                    var count = Math.Min(bufferSize, mesage.Length - offset);
+                    port.Write(mesage, offset, count);
+                    offset += count;
+                    Console.WriteLine($"Sent {count} bytes.");
+                    if (offset >= mesage.Length)
+                        break;
+                }
+                port.Write(new byte[] { 0xFF, 0xFF }, 0, 2); //End of message
+                var high = port.ReadByte();
+                var low = port.ReadByte();
+                var transmittedBytes = high + low << 8;
+                Console.WriteLine($"Confirmed {transmittedBytes} bytes.");
+                var end = port.ReadExisting();
+                Console.WriteLine($"End {end}");
                 port.Close();
             }
         }
